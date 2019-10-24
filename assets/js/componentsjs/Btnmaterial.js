acd49700ff47e515fc3a9bdd006100b444fe602e
Mart Umbao <mart_umbao@commude.ph>
2019-10-24 14:48:02 +0800

    g

---
+++ assets/js/componentsjs/Btnmaterial.js
@@ -0,0 +1,43 @@
+// Material Buttons
+let btns_material = document.querySelectorAll('.btn_m');
+
+btns_material.forEach(btns => {
+      btns.addEventListener('click', function (e) {
+            // e.preventDefault();
+            // console.log(e.target)
+            myFuncBtn_m(e);
+      })
+});
+
+// Functions
+function myFuncBtn_m(e) {
+      e.preventDefault();
+      let target_el = e.target;
+
+      let rect = target_el.getBoundingClientRect();
+
+      let ripple_el = target_el.querySelector('.ripple');
+      try {
+            ripple_el.remove();
+      } catch (error) {
+
+      }
+
+      ripple_el = document.createElement('span');
+      ripple_el.classList.add('ripple');
+      ripple_el.style.height = Math.max(rect.height, rect.width) + 'px';
+      ripple_el.style.width = Math.max(rect.width, rect.height) + 'px';
+
+      target_el.appendChild(ripple_el);
+      var top = e.pageY - rect.top - ripple_el.offsetHeight / 2 - document.body.scrollTop;
+      var left = e.pageX - rect.left - ripple_el.offsetWidth / 2 - document.body.scrollLeft;
+
+      ripple_el.style.top = top + 'px';
+      ripple_el.style.left = left + 'px';
+      return false;
+
+      // ripple_el.style.width = Math.max(rect.width, rect.height) + 'px';
+
+      // console.log(rect)
+
+}
